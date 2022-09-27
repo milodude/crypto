ca500027a66e3f773ba4c2c895df80adb3057c75
Matias Raverta <milodude023@gmail.com>
2022-09-27 09:34:04 -0300

    [CRP-05]Added base use case class to utilize it in all of applications use cases

---
+++ lib/core/use_case/use_case.dart
@@ -0,0 +1,9 @@
+import 'package:crypto/core/error/error.dart';
+import 'package:dartz/dartz.dart';
+
+/// UseCase
+///
+/// Base class for use cases in order to let the use case decide which type returns
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
