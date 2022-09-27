4deee17986d6738e307551dd8bcc9b3ff4a434d2
Matias Raverta <milodude023@gmail.com>
2022-09-27 11:26:46 -0300

    [CRP-15]Added nft repository data source to data layer

--- lib/features/crypto_search/data/data_source/nft_repository_data_source.dart
+++ lib/features/crypto_search/data/data_source/nft_repository_data_source.dart
@@ -1,5 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:crypto_app/core/error/error.dart';
+import 'package:crypto_app/core/providers/url_provider.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:crypto_app/features/crypto_search/data/model/nft_model.dart';
 
 abstract class NftRepositoryDataSource {
   Future<List<NftModel>> getNftList(String address);
+}
+
+class NftDataSource implements NftRepositoryDataSource {
+  final http.Client httpClient;
+  final UrlProvider urlProvider;
+
+  NftDataSource({
+    required this.httpClient,
+    required this.urlProvider,
+  });
+
+  @override
+  Future<List<NftModel>> getNftList(String address) async {
+    Uri uri = urlProvider.getUrl('/actions/scan/$address', {});
+
+    var response = await httpClient.get(uri, headers: {
+      'Content-type': 'application/json; charset=utf-8',
+      'Accept': '*/*',
+      'Access-Control-Allow-Origin': '*',
+    });
+    if (response.statusCode == 200) {
+      var decodedJson = json.decode(response.body);
+      List<NftModel> nftList = <NftModel>[];
+      for (Map<String, dynamic> item in decodedJson) {
+        var nft = NftModel.fromJson(item);
+        nftList.add(nft);
+      }
+      return nftList;
+    } else {
+      throw ServerFailure('Something went wrong while requesting photo list');
+    }
+  }
 }
---
+++ test/features/crypto_search/data/data_source/nft_repository_data_source_test.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:crypto_app/core/error/error.dart';
+import 'package:crypto_app/core/providers/url_provider.dart';
+import 'package:crypto_app/features/crypto_search/data/data_source/nft_repository_data_source.dart';
+import 'package:crypto_app/features/crypto_search/data/model/nft_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+import 'nft_repository_data_source_test.mocks.dart';
+
+@GenerateMocks([http.Client, UrlProvider])
+void main() {
+  final MockUrlProvider mockUrlProvider = MockUrlProvider();
+  final MockClient mockClient = MockClient();
+
+  final NftDataSource nftDataSource =
+      NftDataSource(httpClient: mockClient, urlProvider: mockUrlProvider);
+  const String validAddress = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045';
+  group('Nft repository data source tests: ', () {
+    final tNftListResponse =
+        json.decode(fixture('crypto_search/crypto_search_valid_nft_list.json'));
+    List<NftModel> _getList() {
+      List<NftModel> nftList = <NftModel>[];
+      for (Map<String, dynamic> item in tNftListResponse) {
+        var photo = NftModel.fromJson(item);
+        nftList.add(photo);
+      }
+      return nftList;
+    }
+
+    void setUpHttpCallSuccess200() {
+      final uri = UrlProvider().getUrl('/actions/scan/$validAddress', {});
+      when(
+        mockClient.get(
+          uri,
+          headers: {
+            'Content-type': 'application/json; charset=utf-8',
+            'Accept': '*/*',
+            'Access-Control-Allow-Origin': '*',
+          },
+        ),
+      ).thenAnswer(
+        (_) async => Future.value(
+          http.Response(
+            fixture('crypto_search/crypto_search_valid_nft_list.json'),
+            200,
+            headers: {
+              HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8',
+            },
+          ),
+        ),
+      );
+
+      when(mockUrlProvider.getUrl(any, any))
+          .thenAnswer((realInvocation) => uri);
+    }
+
+    testWidgets(
+        'should perform a get request for a photo list with application/json header',
+        (tester) async {
+      //Arrange
+      setUpHttpCallSuccess200();
+
+      //Act
+      await nftDataSource.getNftList(validAddress);
+      //Assert
+      verify(mockClient.get(any, headers: {
+        'Content-type': 'application/json; charset=utf-8',
+        'Accept': '*/*',
+        'Access-Control-Allow-Origin': '*',
+      }));
+    });
+
+    testWidgets('should get a list of nft when requesting data to the API',
+        (tester) async {
+      //Arrange
+      setUpHttpCallSuccess200();
+      //Act
+      var result = await nftDataSource.getNftList(validAddress);
+      //Assert
+      expect(result, equals(_getList()));
+    });
+
+    test(
+        'Should throw a serverException when the respond is 404 or other when getting a list of nft',
+        () async {
+      //Arrange
+      final uri = UrlProvider().getUrl('/actions/scan/$validAddress', {});
+      when(mockUrlProvider.getUrl(any, any))
+          .thenAnswer((realInvocation) => uri);
+
+      when(mockClient.get(uri, headers: anyNamed('headers'))).thenAnswer(
+        (realInvocation) async =>
+            http.Response('Something went wrong while getting clients', 404),
+      );
+      //Act
+      final call = nftDataSource.getNftList('Some Invalid address');
+      //Assert
+      expect(() => call, throwsA(const TypeMatcher<ServerFailure>()));
+    });
+  });
+}
