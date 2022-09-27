5deb786d0c21056196ab90521fa594f7415c91c7
Matias Raverta <milodude023@gmail.com>
2022-09-27 12:25:44 -0300

    [CRP-17]Added new constants to avoid magic strings.

---
+++ lib/core/constants/animations/animations.dart
@@ -0,0 +1,2 @@
+const String cryptoSearchPageImageUrl =
+    'https://assets7.lottiefiles.com/packages/lf20_gjmecwii.json';
---
+++ lib/core/constants/search_nft_page.dart/text_constants.dart
@@ -0,0 +1,3 @@
+const String smallDescriptiveText = 'Just type the crypto address below!';
+const String addressInputLabelText = 'Crypto Address';
+const String buttonText = 'Proceed with search';
