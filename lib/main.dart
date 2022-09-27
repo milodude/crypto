447dab463a9946b8e6f07af823c0eb8d595eb2e5
Matias Raverta <milodude023@gmail.com>
2022-09-27 13:16:47 -0300

    [CRP-21]Added MultiBlocProvider in main and renamed MyApp for CryptoApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
+import 'package:crypto_app/features/crypto_search/presentation/bloc/nft/nft_bloc.dart';
 import 'package:crypto_app/features/crypto_search/presentation/page/search_nft_page.dart';
+import 'package:crypto_app/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AppState());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NftBloc>(create: (_) => sl<NftBloc>()),
+      ],
+      child: const CryptoApp(),
+    );
+  }
+}
+
+class CryptoApp extends StatelessWidget {
+  const CryptoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
